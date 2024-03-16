62700e7cead376ecd2d1ad3d9ef5037b5c7805f6
Devin Khun <lightningowl2004@gmail.com>
2024-03-15 22:34:33 -0700

    Add Project 3, Part 2 features

--- README.md
+++ README.md
@@ -1,3 +1,69 @@
+# Web Development Project 3 - *GolfPro Learning*
+
+Submitted by: **Devin Khun**
+
+This web app: **A web app that displays a collection of flashcards related to the rules of Golf.**
+
+Time spent: **6** hours spent in total
+
+## Required Features
+
+The following **required** functionality is completed:
+
+- [x] **The user can enter their guess in a box before seeing the flipside of the card**
+- [x] **Clicking on a submit button shows visual feedback about whether the answer was correct or incorrect**
+- [x] **A back button is displayed on the card and can be used to return to the previous card in a set sequence**
+- [x] **A next button is displayed on the card and can be used to navigate to the next card in a set sequence**
+
+The following **optional** features are implemented:
+
+- [x] A shuffle button is used to randomize the order of the cards
+- [x] A user's answer may be counted as correct even when it is slightly different from the target answer
+- [x] A counter displays the user's current and longest streak of correct responses
+- [x] A user can mark a card that they have mastered and have it removed from the pool of answers as well as added to a list of mastered cards
+
+The following **additional** features are implemented:
+
+* [ ] Shuffle only cards that have not been viewed
+* [ ] Progress bar that tracks the user's learning progress
+* [ ] Different levels of mastery status for each flashcard
+* [ ] Bookmark or star certain flashcards for more review
+* [ ] Tagging flashcards into different categories
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories:
+
+<img src='https://i.imgur.com/Cn1K95T.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+
+<!-- Replace this with whatever GIF tool you used! -->
+GIF created with [LICEcap](https://www.cockos.com/licecap/)
+<!-- Recommended tools:
+[Kap](https://getkap.co/) for macOS
+[ScreenToGif](https://www.screentogif.com/) for Windows
+[peek](https://github.com/phw/peek) for Linux. -->
+
+## Notes
+
+Some challenges I encountered was finding a way to check and verify the user's inputted answer against the actual answer of the flashcard. I figured that the best way to approach this was to use a fuzzy search where it would show the user a percentage based on the correctness of the answer rather than showing right or wrong. The function would go through each character of the answer and keep track of how many characters were correct. The shuffle button was also an interesting approach since I took the array of flashcards and essentially randomized the order of the cards.
+
+## License
+
+    Copyright [2024] [Devin Khun]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+
 # Web Development Project 2 - *GolfPro Leaning*
 
 Submitted by: **Devin Khun**
--- src/App.jsx
+++ src/App.jsx
@@ -16,21 +16,29 @@ const App = () => {
   const [cardNumber, setCardNumber] = useState(0);
   const [cardStack, setCardStack] = useState(flashCards[0].stackCard);
   const [cardAnswer, setCardAnswer] = useState(false);
+  const [userInput, setUserInput] = useState("");
+  const [userAnswer, setUserAnswer] = useState(-1);
+  const [correctAnswers, setCorrectAnswers] = useState(0);
 
   const maxCards = cardStack.length;
 
-  const showAnswer = () => { setCardAnswer(!cardAnswer); }
+  const showAnswer = () => { setCardAnswer(!cardAnswer) };
 
   const shuffle = () => {
     setCardStack(randomize(flashCards[0].stackCard));
     setCardNumber(0);
     setCardAnswer(false);
+    setUserInput("");
+    setUserAnswer(-1);
+    setCorrectAnswers(0);
   }
 
   const nextCard = () => {
     if (cardNumber + 1 < maxCards) {
       setCardNumber(cardNumber + 1);
       setCardAnswer(false)
+      setUserInput("");
+      setUserAnswer(-1);
     }
   };
 
@@ -38,15 +46,54 @@ const App = () => {
     if (cardNumber > 0) {
       setCardNumber(cardNumber - 1);
       setCardAnswer(false);
+      setUserInput("");
+      setUserAnswer(-1);
     }
   };
-  
+
+  const checkAnswer = () => {
+    let answer = cardStack[cardNumber].answer.toLowerCase();
+    let userAnswer = userInput.toLowerCase();
+    let match = 0;
+
+    let answerCharCounts = {};
+    let userCharCounts = {};
+
+    for (let char of answer) {
+      answerCharCounts[char] = (answerCharCounts[char] || 0) + 1;
+    }
+
+    for (let char of userAnswer) {
+      userCharCounts[char] = (userCharCounts[char] || 0) + 1;
+    }
+
+    for (let char in answerCharCounts) {
+      if (userCharCounts[char]) {
+        match += Math.min(answerCharCounts[char], userCharCounts[char]);
+      }
+    }
+
+    // answer.forEach((char) => {
+    //   if (userAnswer.includes(char)) {
+    //     match += 1;
+    //   }
+    // });
+
+    if (Math.floor(match/answer.length * 100) > 80) {
+      setCorrectAnswers(correctAnswers + 1);
+    }
+
+    console.log("Characters Matched:", match, "Answer Length:", answer.length, "User Anwer:", Math.floor(match/answer.length * 100));
+    setUserAnswer(Math.floor(match/answer.length * 100));
+  };
+
   return (
     <div className="App">
 
       <h1 className='card-set-title'>{flashCards[0].stackTitle}</h1>
       <h2 className='card-set-description'>{flashCards[0].stackDescription}</h2>
       <h4 className="card-set-description">Number of Cards: {maxCards}</h4>
+      <div className="card-set-description">Correct Answers: {correctAnswers}</div>
 
       <button className="card" onClick={showAnswer}>
         <div className={"flip-card" + (cardAnswer ? " showFlip" : "")}>
@@ -63,13 +110,24 @@ const App = () => {
           </div>
         </div>
       </button>
-      
+
+      <div className="answer-input">
+        <div className="answer-title">{(userAnswer > -1 ? `Your answer is ${userAnswer}% correct` : "Enter your answer")}</div>
+        <input className="answer-box" type="text" placeholder="Enter Answer" value={userInput}
+          onChange={(event) => {
+            setUserInput(event.target.value);
+            setUserAnswer(-1);
+          }}
+        />
+        <button className="check-answer-button" onClick={checkAnswer}>Check Answer</button>
+      </div>
+
       <div id="card-nav">
         <button onClick={prevCard} className={"card-navbutton " + (cardNumber > 0 ? "" : "button-inactive")}>{"<--"}</button>
         <div id="card-count">{cardNumber + 1} / {maxCards}</div>
         <button onClick={nextCard} className={"card-navbutton " + (cardNumber + 1 < maxCards ? "" : "button-inactive")}>{"-->"}</button>
       </div>
-      <button className='card-navbutton' onClick={shuffle}>Shuffle</button>
+      <button className='card-navbutton' onClick={shuffle}>Restart & Shuffle</button>
       
     </div>
   )
